d697e8bf75d7375dd4c5f64ba3e7088de638467c
SoorajSNBlaze333 <SoorajSNBlaze333@gmail.com>
2019-01-20 13:46:06 +0530

    sample

---
+++ src/Containers/Chatscreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+class Chatscreen extends React.Component{
+    render(){
+        return(<div><NavLink to="/chatscreen"/>Chatscreen</div>)
+    }
+}
+export default Chatscreen;
---
+++ src/Containers/index.js
@@ -0,0 +1,8 @@
+import Login from './Login';
+import Chatscreen from './Chatscreen';
+import '../Stylesheets/App.css'
+
+export {
+    Login,
+    Chatscreen
+}
