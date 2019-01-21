4c3d5302d96cbee623d6652b63cb860fe64a7b3a
SoorajSNBlaze333 <SoorajSNBlaze333@gmail.com>
2019-01-21 20:54:52 +0530

    save

--- src/Containers/Chatscreen.js
+++ src/Containers/Chatscreen.js
@@ -59,8 +59,11 @@ class Chatscreen extends React.Component{
                 </Row>
                 <div><button onClick={()=>this.logout()}>Test Logout</button></div>
                 <Row>
-                    <Col offset={1} span={21}><Input placeholder="Type message here" type="text"/></Col>
-                    <Col span={1}><Button type="primary">Send</Button></Col>
+                    <Card>
+                        <Col span={23}><Input placeholder="Type message here" type="text"/></Col>
+                        <Col span={1}><Button type="primary">Send</Button></Col>
+                    </Card>
+                    
                 </Row>
                 </div>)
         }
